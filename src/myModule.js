0c9ceb28e188ffd942902175aa35ec55c1e4f165
Jeff P <yeffrey@gmail.com>
2020-12-29 22:43:55 -0600

    fix?

--- src/myModule.js
+++ src/myModule.js
@@ -8,14 +8,16 @@ import {
 
 export const pickArray = (array) => array[Math.floor(Math.random() * array.length)]
 
-export const randomPlace = () => {
+const randomPlace = () => {
   const adj = pickArray(placeDescribe)
   const place = pickArray(places)
   return camelCase(`${adj} ${place}`)
 }
 
-export const randomAnimal = () => {
+const randomAnimal = () => {
   const adj = pickArray(adjectives)
   const animal = pickArray(animals)
   return camelCase(`${adj} ${animal}`)
 }
+
+export default { randomPlace, randomAnimal }
