7c67b320375c2b315a0ea61a299015794902be0a
Jeff P <yeffrey@gmail.com>
2020-12-29 22:17:00 -0600

    update

--- src/myModule.js
+++ src/myModule.js
@@ -1,7 +1,29 @@
-const myModule = {}
+import pascalcase from 'pascalcase'
+import {
+  places,
+  placeDescribe,
+  animals,
+  adjectives
+} from './words'
 
-myModule.ping = () => {
+export const pickArray = (array) => array[Math.floor(Math.random() * array.length)]
+
+const niceRandom = {}
+
+niceRandom.ping = () => {
   return 'pong'
 }
 
-export default myModule
+niceRandom.place = () => {
+  const adj = pickArray(placeDescribe)
+  const place = pickArray(places)
+  return pascalcase(`${adj} ${place}`)
+}
+
+niceRandom.animal = () => {
+  const adj = pickArray(adjectives)
+  const animal = pickArray(animals)
+  return pascalcase(`${adj} ${animal}`)
+}
+
+export default niceRandom
